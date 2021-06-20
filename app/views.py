1de13c3edd63bd7c3a614ce9f494cddc02d71b5a
Shubham <thorgoyal@gmail.com>
2021-06-20 20:42:52 +0530

    final

--- app/views.py
+++ app/views.py
@@ -5,20 +5,16 @@ import json
 import requests
 # Create your views here.
 def home(request):   
-
     count = 0
-    temp = []
+    temp = []    
     api_url = 'https://reqres.in/api/users?page='
     for i in range(1,13):
         response=requests.get(api_url + str(i)).json()  
-        # temp.append(response)
-        print('\n',response[0])
-
-    context = {
-        'title': 'Shubham , lets start api',    
-        # 'response':temp
-    }
-    return HttpResponse(response)
+        temp.append(response)
+        count = count + len(response['data'])
+        print(len(response['data']))  
+        # print('\n',response[i])  
+    return HttpResponse(count)
     
 
 
